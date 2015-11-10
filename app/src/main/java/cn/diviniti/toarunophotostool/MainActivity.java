5d074a5b30ed2a570683c6035f544b1e8f8ec584
vango <daikaiquan46@gmail.com>
2015-11-10 16:20:54 +0800

    Initial commit

---
+++ app/src/main/java/cn/diviniti/toarunophotostool/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.diviniti.toarunophotostool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
